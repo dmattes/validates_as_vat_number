70b99a147f36aac061c9aaa888ac50ff95048a56
Daniel Mattes <daniel.mattes@gmx.de>
2011-02-02 14:48:55 +0100

    modified regexp to match http://ec.europa.eu/taxation_customs/vies/faqvies.do#item11 in detail and remove spaces

--- lib/validates_as_vat_number.rb
+++ lib/validates_as_vat_number.rb
@@ -8,15 +8,16 @@ module Develon
         :allow_nil => false 
       }
       configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-      
-      structures = { 'AT' => /^ATU\w{8}$/, 'BE' => /^BE0\d{9}$/, 'BG' => /^BG\d{9,10}$/, 'CY' => /^CY\w{8}L$/,
-                     'CZ' => /^CZ\d{8,10}$/, 'DE' => /^DE\d{9}$/, 'DK' => /^DK\d{2}\s\d{2}\s\d{2}\s\d{2}$/,
-                     'EE' => /^EE\d{9}$/, 'EL' => /^EL\d{9}$/, 'ES' => /^ESX\d{7}X$/, 'FI' => /^FI\d{8}$/, 'FR' => /^FR\w{2}\s\d{9}$/,
-                     'GB' => /^GB(\d{3}\s\d{4}\s\d{2}|\d{3}\s\d{4}\s\d{2}\s\d{3}|GD\d{3}|HA\d{3})$/,
-                     'HU' => /^HU\d{8}$/, 'IE' => /^IE\wS\w{5}L$/, 'IT' => /^IT\d{11}$/, 'LT' => /^LT(\d{9}|\d{12})$/,
-                     'LU' => /^LU\d{8}$/, 'LV' => /^LV\d{12}$/, 'MT' => /^MT\d{8}$/, 'NL' => /^NL\w{9}B\w{2}$/,
-                     'PL' => /^PL\d{10}$/, 'PT' => /^PT\d{9}/, 'RO' => /^RO\d{2,10}$/, 'SE' => /^SE\d{12}$/, 'SI' => /^SI\d{8}$/,
-                     'SK' =>/^SK\d{10}$/ }
+
+      structures = {'AT' => /^ATU\w{8}$/, 'BE' => /^BE0\d{9}$/, 'BG' => /^BG\d{9,10}$/, 'CY' => /^CY\d{8}\w{1}$/,
+                    'CZ' => /^CZ\d{8,10}$/, 'DE' => /^DE\d{9}$/, 'DK' => /^DK\d{8}$/, 'EE' => /^EE\d{9}$/,
+                    'EL' => /^EL\d{9}$/, 'ES' => /^ES(\d{8}[A-Za-z]{1}|[A-Za-z]{1}\d{8}|[A-Za-z]{1}\d{7}[A-Za-z]{1})$/,
+                    'FI' => /^FI\d{8}$/, 'FR' => /^FR\w{2}\d{9}$/, 'GB' => /^GB(\d{9}|\d{12}|(GD|HA)\d{3})$/,
+                    'HU' => /^HU\d{8}$/, 'IE' => /^IE\d{1}[\w+*]{1}\d{5}[A-Za-z]{1}$/, 'IT' => /^IT\d{11}$/,
+                    'LT' => /^LT(\d{9}|\d{12})$/, 'LU' => /^LU\d{8}$/, 'LV' => /^LV\d{11}$/, 'MT' => /^MT\d{8}$/,
+                    'NL' => /^NL\d{9}B\d{2}$/, 'PL' => /^PL\d{10}$/, 'PT' => /^PT\d{9}/, 'RO' => /^RO\d{2,10}$/,
+                    'SE' => /^SE\d{12}$/, 'SI' => /^SI\d{8}$/, 'SK' =>/^SK\d{10}$/
+      }
 
       validates_each(attr_names,configuration) do |record, attr_name, value|
         country = country_code(value)
